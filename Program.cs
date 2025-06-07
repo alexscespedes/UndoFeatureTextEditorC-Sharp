51a8755f2b2360c0dd45e058d8571532a54ab01b
alexscespedes <alexandersenc99@gmail.com>
2025-06-07 09:45:54 -0400

    Set Up the Undo Stack and Perform Actions

--- Program.cs
+++ Program.cs
@@ -2,10 +2,40 @@
 
 class Program
 {
+    static Stack<EditorAction> undoStack = new Stack<EditorAction>();
     static void Main(string[] args)
     {
-        var editorAction = new EditorAction(ActionType.Insert, "Hello world", 1);
+        PerfomAction(new EditorAction(ActionType.Insert, "Hello", 0));
+        PerfomAction(new EditorAction(ActionType.Insert, "World", 5));
+        PerfomAction(new EditorAction(ActionType.Insert, "o", 4));
 
-        Console.WriteLine(editorAction.ToString());
+        Console.WriteLine("\nAll actions performed. Press any key to undo them...");
+        Console.ReadKey();
+
+        UndoLastAction();
+        UndoLastAction();
+        UndoLastAction();
+
+        UndoLastAction();
+
+        Console.WriteLine("\nDone.");
+    }
+
+    static void PerfomAction(EditorAction action)
+    {
+        undoStack.Push(action);
+        Console.WriteLine($"Performed action: {action}");
+    }
+
+    static void UndoLastAction()
+    {
+        if (undoStack.Count == 0)
+        {
+            Console.WriteLine("Nothing left to undo.\n");
+            return;
+        }
+
+        var lastAction = undoStack.Pop();
+        Console.WriteLine($"Undo: {lastAction}");
     }
 }
